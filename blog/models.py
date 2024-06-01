d747230d924fcb63cb49c50aa235e1da6a175983
jklee3409 <jklee3409@khu.ac.kr>
2024-06-01 14:25:18 +0900

    Number guessing game

--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-  author = models.ForeignKey(
-    settings.AUTH_USER_MODEL,
-    on_delete=models.CASCADE)
+  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   title = models.CharField(max_length=200)
   text = models.TextField()
-  created_date = models.DateField(
-    default=timezone.now)
-  published_date = models.DateTimeField(
-    blank=True, null=True)
+  created_date = models.DateField(default=timezone.now)
+  published_date = models.DateTimeField(blank=True, null=True)
   image = models.ImageField(upload_to='intruder_image/%Y/%m/%d/', default='intruder_image/default_error.png')
 
   def publish(self):
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from rest_framework import viewsets
+from .serializers import PostSerializer
 
 def post_list(request):
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -13,18 +15,18 @@ def post_detail(request, pk):
   post = get_object_or_404(Post, pk=pk)
   return render(request, 'blog/post_detail.html', {'post':post})
 
-def post_new(request):
-  if request.method == "POST":
-    form = PostForm(request.POST)
-    if form.is_valid():
-      post = form.save(commit=False)
-      post.author = request.user
-      post.published_date = timezone.now()
-      post.save()
-      return redirect('post_detail', pk=post.pk)
-  else:
-    form = PostForm
-  return render(request, 'blog/post_edit.html', {'form':form})
+def post_new(request):  
+    if request.method == "POST":
+      form = PostForm(request.POST)
+      if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+      form = PostForm
+    return render(request, 'blog/post_edit.html', {'form':form})
 
 def post_edit(request, pk):
   post = get_object_or_404(Post, pk=pk)
@@ -39,3 +41,10 @@ def post_edit(request, pk):
   else:
     form = PostForm(instance=post)
   return render(request, 'blog/post_edit.html', {'form': form})
+
+class IntruderImage(viewsets.ModelViewSet):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+
+def js_test(request):
+  return render(request, 'blog/js_test.html', {})
