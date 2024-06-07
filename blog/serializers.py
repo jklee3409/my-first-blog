855181ceaf2e4a5927a678fe84468fa8a3bc2691
jklee3409 <jklee3409@khu.ac.kr>
2024-06-08 00:51:40 +0900

    Android image trnasfer success

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,12 +1,26 @@
-from blog.models import Post
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Post
+from django.core.files.base import ContentFile
+import base64
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    image = serializers.CharField(allow_null=True, required=False)
 
     class Meta:
         model = Post
         fields = ('author', 'title', 'text', 'created_date', 'published_date', 'image')
+
+    def create(self, validated_data):
+        # 이미지 데이터를 디코딩하여 모델에 저장
+        image_data = validated_data.pop('image', None)
+        if image_data:
+            format, imgstr = image_data.split(';base64,')
+            ext = format.split('/')[-1]
+            decoded_image = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+            validated_data['image'] = decoded_image
+        return super().create(validated_data)
+
     
     
--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-from django.shortcuts import redirect
 from rest_framework import viewsets, status
-from .serializers import PostSerializer
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import PostSerializer
+import base64
+from django.core.files.base import ContentFile
 
 class BlogImage(viewsets.ModelViewSet):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
-
-  def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
+  
 def js_test(request):
-  return render(request, 'blog/js_test.html', {})
+    return render(request, 'blog/js_test.html', {})
 
 def post_list(request):
-  posts = Post.objects.order_by('created_date')
-  return render(request, 'blog/post_list.html', {'posts':  posts})
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-  post = get_object_or_404(Post, pk=pk)
-  return render(request, 'blog/post_detail.html', {'post':post})
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -38,7 +33,6 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.image = request.POST('image')
-            post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
@@ -47,16 +41,15 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method== "POST":
-      form = PostForm(request.POST, instance=post)
-      if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.published_date= timezone.now()
-        post.image = request.POST['image']
-        post.save()
-        return redirect('post_detail', pk=post.pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.image = request.POST.get('image')
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
-      form = PostForm(instance=post)
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
